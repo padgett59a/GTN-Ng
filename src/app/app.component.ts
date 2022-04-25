944cf5a77bfbddb94e8c849bd95918e0dc9c5832
Anthony Padgett <apadgett.ap59@gmail.com>
2022-04-25 19:30:25 -0400

    Added Home and User components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { NbMenuItem, NbSidebarService } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'GTN-Ng';
+  items: NbMenuItem[] = [
+    {
+      title: 'Home',
+      icon: 'home-outline',
+      link: '/home',
+      home: true
+    },
+    {
+      title: 'Users',
+      icon: 'people-outline',
+      link: '/users'
+    }
+  ];
+
+  constructor(private readonly sidebarService: NbSidebarService) {
+  }
+  
+  toggleSidebar(): boolean {
+    this.sidebarService.toggle();
+    return false;
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NbIconModule, NbLayoutModule, NbMenuModule, NbSidebarModule, NbThemeModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbIconModule,               
+    NbSidebarModule.forRoot(),  
+    NbMenuModule.forRoot(),     
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
